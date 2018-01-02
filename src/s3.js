e0527f13ec47204c47df2eb6df04458433886ee7
Rodrigo Auler <rcauler@infosites.com.br>
2018-01-02 11:26:15 -0200

    s3

--- src/s3.js
+++ src/s3.js
@@ -22,12 +22,33 @@ exports.getSignedUrl = function (bucket, key, contentType, contentLength) {
     return s3.getSignedUrl('putObject', params)
 }
 
+exports.getObject = function (bucket, key) {
+    var params = {
+        Bucket: bucket,
+        Key:  key
+    }
+
+    return s3.getObjectAsync(params)
+}
+
 exports.putJson = function (bucket, key, json) {
     var params = {
         Bucket: bucket,
         Key:  key,
         Body: JSON.stringify(json),
         ContentType: 'application/json',
+        ACL: 'public-read'
+    }
+
+    return s3.putObjectAsync(params)
+}
+
+exports.putFile = function (bucket, key, file, contentFile) {
+    var params = {
+        Bucket: bucket,
+        Key:  key,
+        Body: file,
+        ContentType: contentFile,
         ACL: 'public-read'
     }
 
